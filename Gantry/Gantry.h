54154baac487e93192a790740636f10ab873406f
Isaac Getto <isaac.getto@gmail.com>
2015-10-28 11:06:03 -0400

    provide gantry with motor shield address and stepper port

--- Gantry/Gantry.h
+++ Gantry/Gantry.h
@@ -9,14 +9,17 @@
 
 class Gantry {
     public:
-        Gantry(int pinX, int pinY);
+        Gantry( int xMotorShieldAddr, int xStepperPort,
+                int yMotorShieldAddr, int yStepperPort);
         void move(float x, float y, float speed = DEFAULT_SPEED);
 
     private:
         const float DEFAULT_SPEED = 10;
 
-        int _pinX;
-        int _pinY;
+        int _xMotorShieldAddr;
+        int _yMotorShieldAddr;
+        int _xStepperPort;
+        int _yStepperPort;
 };
 
 #endif
