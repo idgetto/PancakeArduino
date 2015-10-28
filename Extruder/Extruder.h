9f93e4e5bed1cd311b7d3fd0232924693d5da131
Isaac Getto <isaac.getto@gmail.com>
2015-10-27 22:48:50 -0400

    define Extruder api

--- Extruder/Extruder.h
+++ Extruder/Extruder.h
@@ -3,10 +3,21 @@
     Author: Isaac Getto
     Date: 10/27/15
 */
+
 #ifndef EXTRUDER_H
 #define EXTRUDER_H
 
 class Extruder {
+    public:
+        Extruder(int pumpPin, int solenoidPin);
+        void extrude(float speed = DEFAULT_SPEED);
+        void stop();
+
+    private:
+        const float DEFAULT_SPEED = 10;
+
+        int _pumpPin;
+        int _solenoidPin;
 };
 
 #endif
