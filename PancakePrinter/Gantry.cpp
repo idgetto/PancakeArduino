eb926ae662c77928d86c01943c4641b08ea13a3e
Isaac Getto <isaac.getto@gmail.com>
2015-11-03 00:49:55 -0500

    make Gantry implement initializable

--- PancakePrinter/Gantry.cpp
+++ PancakePrinter/Gantry.cpp
@@ -5,7 +5,18 @@ Gantry::Gantry( int xMotorShieldAddr, int xStepperPort,
                     _xMotorShieldAddr{xMotorShieldAddr},
                     _xStepperPort{xStepperPort},
                     _yMotorShieldAddr{yMotorShieldAddr},
-                    _yStepperPort{yStepperPort} { }
+                    _yStepperPort{yStepperPort},
+                    _xMotorShield{_xMotorShieldAddr},
+                    _yMotorShield{_yMotorShieldAddr} {
+    _xStepper = _xMotorShield.getStepper(STEPS_PER_REV, _xStepperPort);
+    _yStepper = _yMotorShield.getStepper(STEPS_PER_REV, _yStepperPort);
+}
 
-void Gantry::init() {
+void Gantry::move(float x, float y, float speed) {
+    assertInitialized();
+}
+
+void Gantry::_init() {
+    _xMotorShield.begin();
+    _yMotorShield.begin();
 }
--- PancakePrinter/Gantry.h
+++ PancakePrinter/Gantry.h
@@ -8,21 +8,34 @@
 #define GANTRY_H
 
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_PWMServoDriver.h"
 
-class Gantry {
+#include "Initializable.h"
+
+class Gantry : public Initializable {
     public:
         Gantry( int xMotorShieldAddr, int xStepperPort,
                 int yMotorShieldAddr, int yStepperPort);
-        void init();
         void move(float x, float y, float speed = Gantry::DEFAULT_SPEED);
+
+    protected:
+        void _init();
 
     private:
         static const float DEFAULT_SPEED = 10;
+        static const unsigned STEPS_PER_REV = 200;
 
         int _xMotorShieldAddr;
         int _yMotorShieldAddr;
         int _xStepperPort;
         int _yStepperPort;
+
+        Adafruit_MotorShield _xMotorShield;
+        Adafruit_MotorShield _yMotorShield;
+        Adafruit_StepperMotor *_xStepper;
+        Adafruit_StepperMotor *_yStepper;
 };
 
 #endif
